31b9b1a9f175bff7dae5484b2fa9707804ffd925
Ryan Emerson <remerson@ibm.com>
2026-03-10 17:07:38 +0000

    Build images on deployment

--- spiffe/skupper-spiffe-federation-mtls/deploy/deploy-all.sh
+++ spiffe/skupper-spiffe-federation-mtls/deploy/deploy-all.sh
@@ -2,7 +2,7 @@
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#${SCRIPT_DIR}/init-images.sh
+${SCRIPT_DIR}/init-images.sh
 ${SCRIPT_DIR}/init-spire-public.sh
 ${SCRIPT_DIR}/init-spire-private.sh
 ${SCRIPT_DIR}/init-spire-skupper-federation.sh

2f83c6d35e0ded416fae5f47cf0f358b7fb42137
Ryan Emerson <remerson@ibm.com>
2026-03-10 17:15:40 +0000

    Copy over authz client/server code

--- keycloak/skupper-spiffe-federation/client/src/main/java/com/example/spiffe/client/HelloResource.java
+++ keycloak/skupper-spiffe-federation/client/src/main/java/com/example/spiffe/client/HelloResource.java
@@ -18,21 +18,62 @@ public class HelloResource {
     private static final Logger log = Logger.getLogger(HelloResource.class);
 
     @Inject
-    ZeroTrustContext context;
+    ZeroTrustContext scheduler;
 
     @GET
-    @Path("/hello")
+    @Path("/authorized-hello")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String authorizedHello() {
+        return authorized("/hello/World");
+    }
+
+    @GET
+    @Path("/authorized-admin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String authorizedAdmin() {
+        return authorized("/admin");
+    }
+
+    @GET
+    @Path("/unauthorized-hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String unauthorized() {
+        return unauthorized("/hello/World");
+    }
+
+    @GET
+    @Path("/unauthorized-admin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String unauthorizedAdmin() {
+        return unauthorized("/admin");
+    }
+
+    private String unauthorized(String path) {
         try {
             HttpRequest request = HttpRequest.newBuilder()
-                  .uri(URI.create(context.getServerUrl() + "/hello/World"))
+                  .uri(URI.create(scheduler.getServerUrl() + path))
                   .GET()
                   .build();
-            HttpResponse<String> response = context.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = scheduler.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             return response.statusCode() + ": " + response.body();
         } catch (Exception e) {
-            log.errorf(e, "Request failed: %s", e.getMessage());
+            log.errorf(e, "Unauthorized request failed: %s", e.getMessage());
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    private String authorized(String path) {
+        try {
+            String accessToken = scheduler.fetchAccessToken();
+            HttpRequest request = HttpRequest.newBuilder()
+                  .uri(URI.create(scheduler.getServerUrl() + path))
+                  .header("Authorization", "Bearer " + accessToken)
+                  .GET()
+                  .build();
+            HttpResponse<String> response = scheduler.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            return response.statusCode() + ": " + response.body();
+        } catch (Exception e) {
+            log.errorf(e, "Authorized request failed: %s", e.getMessage());
             return "Error: " + e.getMessage();
         }
     }
